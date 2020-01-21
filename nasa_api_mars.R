3857c862b93e8f35c379eacdbfcc1a921fb186ca
kt <kthobbs@mail.ubc.ca>
2020-01-21 08:12:03 -0800

    formatted response

--- nasa_api_mars.R
+++ nasa_api_mars.R
@@ -4,26 +4,18 @@
 #-------------------------------
 #### Import libraries and data
 #-------------------------------
-library('RJSONIO')
+
 library('jsonlite')
-library('tidyverse')
-library('kableExtra')
+library('purrr')
+library('dplyr')
+library('tidyr')
+
+req <- "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
+mars <- fromJSON(req)
+
+map(mars[1:7], ~unlist(.x[1:6]) %>%
+      bind_rows) %>%
+  bind_rows(.id = "day") %>%
+  pivot_longer(cols = grep("\\.", names(.)), names_sep = "\\.", names_to = c(".value", "var"))
 
 
-request <- "https://api.nasa.gov/insight_weather/?api_key=hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS&feedtype=json&ver=1.0"
-mars <- fromJSON(request, flatten = TRUE)
-
-cnames <- names(mars$`401`)
-
-# restructure for each report
-mars$`407` <- cbind(mars$`407`$AT,
-                    rep(mars$`407`$First_UTC, length(mars$`407`$AT)), 
-                    rep(mars$`407`$Last_UTC, length(mars$`407`$AT)),
-                    mars$`407`$PRE, 
-                    rep(mars$`407`$Season, length(mars$`407`$AT)),
-                    rep(mars$`407`$WD$most_common, length(mars$`407`$AT))
-)
-
-colnames(mars$`407`) <- cnames
-
-mars$`407`
