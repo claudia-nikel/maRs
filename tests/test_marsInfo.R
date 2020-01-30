ee02c822519c338a149750bb08bac8bba8b35e69
Claudia Nikel <claudia-nikel>
2020-01-29 22:13:22 -0800

    <adding more unit tests>

--- tests/test_marsInfo.R
+++ tests/test_marsInfo.R
@@ -6,8 +6,32 @@ test_that("invalid API key", {
 })     
 
 test_that("data types correct", {
-  expect_is(typeof(marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")),'character')
+  x<-marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")
+  expect_is(typeof(x),'character')
 })
+
+
+test_that("correct column names", {
+  x<-c("sol", "atm_temp", "horiz_windspeed", "atm_pres")
+  y<-marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")
+  expect_named(y, x)
+})
+
+test_that("correct type of data in sol column", {
+  x<-marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")
+  expect_equal(typeof(x$sol),'integer')
+})
+
+test_that("output is a data frame", {
+  x<-marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")
+  expect_is(x,'data.frame')
+})
+
+test_that("correct dataframe size", {
+  x<-marsInfo("hecLCNM6NcwAGgGGWSW2xovr0SyYuXiOShVw6GxS")
+  expect_equal(dim(x), c(7, 4))
+})
+
 
 
 #to test if OK or Failed in R console:
